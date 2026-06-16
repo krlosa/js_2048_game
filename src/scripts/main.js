4c5c8e25115743f3cc5ca72a8070f1b6ef9bc9f3
Carlos Alberto <betto.1993@hotmaail.com>
2026-06-16 07:19:45 -0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
+// import Game from '../modules/Game.class';
 // const game = new Game();
 
 // Write your code here
